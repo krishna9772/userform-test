7ce973869d25b3f80099d3d6048fad30b98766f7
Aung Naing Thu <aryalkrishna642@gmail.com>
2022-02-03 02:07:00 +0630

    Route configuration for form submission and token authentication

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+ 
 Route::get('/',HomeComponent::class);
 
 Route::middleware(['auth:sanctum','verified'])->group(function() {
